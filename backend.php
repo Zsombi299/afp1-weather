f5cb4d3a35c00b94deac4194a846b7ead532def0
bazdmeg <fegecskeke@gmail.com>
2025-11-17 21:01:57 +0100

    Fő backend fájl elkészítése
    Létrehoztam a backend.cs nevű fájlt. Elhelyeztem a city.list.json fájlt, amely a város megkereséséhez szükséges.

---
+++ backend.php
@@ -0,0 +1,19 @@
+<?php
+$apiKey = "e2d8124b4ac45c54fcabe703fa7a9492";
+$cityId = "721239";
+$googleApiUrl = "https://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
