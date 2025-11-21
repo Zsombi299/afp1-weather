13ff343ef9c8d3c0c2a5cee23cb73e8686d8facc
bazdmeg <fegecskeke@gmail.com>
2025-11-21 03:20:09 +0100

    Város id megtalálása név alapján
    Megkeresi a város id-jét a JSON fájlból név alapján amit majd a helyhez illő időjárás mutatáshoz használunk fel

--- backend.php
+++ backend.php
@@ -1,6 +1,20 @@
 <?php
+
+function getCityIdFromJson($filename, $cityName) {
+    $jsonString = file_get_contents($filename);
+    $cities = json_decode($jsonString, true);
+    
+    foreach ($cities as $city) {
+        if (isset($city['name']) && strcasecmp($city['name'], $cityName) === 0) {
+            return $city['id'];
+        }
+    }
+    return null;
+}
+
 $apiKey = "e2d8124b4ac45c54fcabe703fa7a9492";
-$cityId = "721239";
+$cityId = getCityIdFromJson('city.list.json', $location['city']);
+//$cityId = "721239";
 $googleApiUrl = "https://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
 
 $ch = curl_init();
@@ -16,6 +30,4 @@ $response = curl_exec($ch);
 curl_close($ch);
 $data = json_decode($response);
 $currentTime = time();
-
-
 ?>
